742a0e6c1a8555e8dfb1e4899e2f2c150e2b336e
rafal.myslek <rafal.myslek@d9fd6366-ad81-11dd-b223-d332c20d7cfc>
2008-12-04 19:53:33 +0000

    Renamed ContentHandlerChain to ContentHandlerManager.

--- webmail/webmail-core/src/main/java/com/myslek/webmail/api/ContentHandlerManager.java
+++ webmail/webmail-core/src/main/java/com/myslek/webmail/api/ContentHandlerManager.java
@@ -6,13 +6,15 @@ import javax.mail.Part;
 
 import com.myslek.webmail.domain.MailPart;
 
-public interface ContentHandlerChain {
-	
+public interface ContentHandlerManager {
+
 	public void addContentHandler(String contentType, ContentHandler handler);
-	
+
 	public Collection<ContentHandler> getContentHandlers();
-	
-	public void fromPartContent(Part part, MailPart mailPart) throws MessageConversionException;
-	
-	public void toPartContent(MailPart mailPart, Part part) throws MessageConversionException;
+
+	public void fromPartContent(Part part, MailPart mailPart)
+			throws MessageConversionException;
+
+	public void toPartContent(MailPart mailPart, Part part)
+			throws MessageConversionException;
 }
