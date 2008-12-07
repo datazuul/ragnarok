f12523409db189f91172c573f43729a7df24f51e
rafal.myslek <rafal.myslek@d9fd6366-ad81-11dd-b223-d332c20d7cfc>
2008-12-07 20:33:46 +0000

    Removed obsolate classes. Annotated service components with EJB annotations.

--- trunk/webmail/webmail-core/src/main/java/com/myslek/webmail/service/MailStoreService.java
+++ trunk/webmail/webmail-core/src/main/java/com/myslek/webmail/service/MailStoreService.java
@@ -2,8 +2,11 @@ package com.myslek.webmail.service;
 
 import java.util.Collection;
 
+import javax.ejb.Local;
+
 import com.myslek.webmail.domain.MailMessage;
 
+@Local
 public interface MailStoreService {
 	
 	public void storeMessages(Collection<MailMessage> messages);
