aefea65774b0de9482fe993b2e6c87ecbcd6417e
rafal.myslek <rafal.myslek@d9fd6366-ad81-11dd-b223-d332c20d7cfc>
2009-01-14 18:53:45 +0000

    ContentHandlers implementation.

--- webmail/webmail-core/src/main/java/com/myslek/webmail/api/BlobContentHandler.java
+++ webmail/webmail-core/src/main/java/com/myslek/webmail/api/BlobContentHandler.java
@@ -1,12 +1,16 @@
 package com.myslek.webmail.api;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.activation.DataHandler;
 import javax.mail.MessagingException;
 import javax.mail.Part;
 import javax.mail.Session;
+import javax.mail.util.ByteArrayDataSource;
 
+import com.myslek.webmail.domain.Content;
 import com.myslek.webmail.domain.MailPart;
 
 public class BlobContentHandler extends AbstractContentHandler {
@@ -21,17 +25,21 @@ public class BlobContentHandler extends AbstractContentHandler {
 	public void fromPartContent(Part part, MailPart mailPart,
 			ContentHandlerManager contentHandlerManager)
 			throws MessageConversionException {
-		InputStream is = null;
+		InputStream in = null;
 		try {
-			is = part.getInputStream();
+			in = part.getInputStream();
+			Content content = new Content();
+			content.setData(getBytes(in));
+			mailPart.setContent(content);
+			mailPart.setFileName(part.getFileName());
 		} catch (IOException e) {
 			throw new MessageConversionException(e);
 		} catch (MessagingException e) {
 			throw new MessageConversionException(e);
 		} finally {
-			if (is != null) {
+			if (in != null) {
 				try {
-					is.close();
+					in.close();
 				} catch (IOException e) {
 					throw new MessageConversionException(e);
 				}
@@ -42,5 +50,24 @@ public class BlobContentHandler extends AbstractContentHandler {
 	public void toPartContent(MailPart mailPart, Part part,
 			Session session, ContentHandlerManager contentHandlerManager)
 			throws MessageConversionException {
+		try {
+			ByteArrayDataSource ds = new ByteArrayDataSource(
+					mailPart.getContent().getData(), mailPart.getContentType());
+			part.setDataHandler(new DataHandler(ds));
+			part.setFileName(mailPart.getFileName());
+		} catch (MessagingException e) {
+			throw new MessageConversionException(e);
+		}
+	}
+	
+	protected byte[] getBytes(InputStream in) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte[] buffer = new byte[4 * 1024];
+		int len;
+		while ((len = in.read(buffer)) != -1) {
+			out.write(buffer, 0, len);
+		}
+		
+		return out.toByteArray();
 	}
 }
